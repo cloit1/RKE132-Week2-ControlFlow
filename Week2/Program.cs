f9c3d3b86e44dbd30e33f395720acdfeb2d5e471
CLoit. <cristo.loit@tktk.ee>
2023-09-11 12:40:45 +0300

    add week2 code

---
+++ Week2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Security;
+
+Console.WriteLine("Hello, world!");
+//uued andmetüübid: int ja char
+//if-statements (control flow)
+
